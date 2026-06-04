1a8da76584926b23c540c189e858090aeff58f28
Sigrið Gyldenkærne Dalsgarð <sigriddalsgard@users.noreply.github.com>
2026-06-04 08:52:06 +0200

    implemented `console.table()` (#23)

--- test.js
+++ test.js
@@ -111,6 +111,137 @@ test('assert', (t) => {
   console.assert(0, 'falsy')
 })
 
+test('table with array of objects', (t) => {
+  t.plan(1)
+
+  const log = {
+    format(value) {
+      return String(value)
+    },
+    info(data) {
+      t.is(
+        data,
+        '┌─────────┬───┬───┐\n' +
+          '│ (index) │ a │ b │\n' +
+          '├─────────┼───┼───┤\n' +
+          '│ 0       │ 1 │ 2 │\n' +
+          '│ 1       │ x │ y │\n' +
+          '└─────────┴───┴───┘'
+      )
+    }
+  }
+
+  const console = new Console(log)
+
+  console.table([
+    { a: 1, b: 2 },
+    { a: 'x', b: 'y' }
+  ])
+})
+
+test('table with properties filter', (t) => {
+  t.plan(1)
+
+  const log = {
+    format(value) {
+      return String(value)
+    },
+    info(data) {
+      t.is(
+        data,
+        '┌─────────┬───┐\n' +
+          '│ (index) │ a │\n' +
+          '├─────────┼───┤\n' +
+          '│ 0       │ 1 │\n' +
+          '│ 1       │ 3 │\n' +
+          '└─────────┴───┘'
+      )
+    }
+  }
+
+  const console = new Console(log)
+
+  console.table(
+    [
+      { a: 1, b: 2 },
+      { a: 3, b: 4 }
+    ],
+    ['a']
+  )
+})
+
+test('table with object', (t) => {
+  t.plan(1)
+
+  const log = {
+    format(value) {
+      return String(value)
+    },
+    info(data) {
+      t.is(
+        data,
+        '┌─────────┬───┐\n' +
+          '│ (index) │ x │\n' +
+          '├─────────┼───┤\n' +
+          '│ a       │ 1 │\n' +
+          '│ b       │ 2 │\n' +
+          '└─────────┴───┘'
+      )
+    }
+  }
+
+  const console = new Console(log)
+
+  console.table({ a: { x: 1 }, b: { x: 2 } })
+})
+
+test('table strips ANSI codes when measuring width', (t) => {
+  t.plan(1)
+
+  const log = {
+    format(value) {
+      return '\x1b[33m' + String(value) + '\x1b[39m'
+    },
+    info(data) {
+      t.is(
+        data,
+        '┌─────────┬───┐\n' +
+          '│ (index) │ a │\n' +
+          '├─────────┼───┤\n' +
+          '│ 0       │ \x1b[33m1\x1b[39m │\n' +
+          '│ 1       │ \x1b[33m2\x1b[39m │\n' +
+          '└─────────┴───┘'
+      )
+    }
+  }
+
+  const console = new Console(log)
+
+  console.table([{ a: 1 }, { a: 2 }])
+})
+
+test('table with primitives falls back to log', (t) => {
+  t.plan(1)
+
+  const log = {
+    info(data) {
+      t.is(data, 'hello')
+    }
+  }
+
+  const console = new Console(log)
+
+  console.table('hello')
+})
+
+test('table with non-array properties throws', async (t) => {
+  t.plan(1)
+
+  const console = new Console({ info() {} })
+
+  await t.exception.all(() => console.table([{ a: 1 }], 'a'), /must be an array/)
+})
+
 test('bound console methods', (t) => {
   t.plan(2)
 
